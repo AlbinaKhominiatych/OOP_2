d4bf7fa368c912bafb9478d6ce763d3b2905b6e0
AlbinaKhominiatych <hominyatichalbina@gmail.com>
2023-11-01 21:02:47 +0200

    ділення

--- main.py
+++ main.py
@@ -24,13 +24,19 @@ class Fraction:
         new_numerator = self.numerator * other.denominator - other.numerator * self.denominator
         new_denominator = self.denominator * other.denominator #спільний знаменник
         return Fraction(new_numerator, new_denominator)
+
     def __mul__(self, other):
         new_numerator = self.numerator * other.numerator
         new_denominator = self.denominator * other.denominator
         return Fraction(new_numerator, new_denominator)
+
+    def __truediv__(self, other):
+        other.denominator, other.numerator = other.numerator, other.denominator
+        return Fraction.__mul__(self, other)
 
 fraction1 = Fraction(4, 7)
 fraction2 = Fraction(5, 8)
 print(fraction1 + fraction2)
 print(fraction1 - fraction2)
 print(fraction1 * fraction2)
+print(fraction1 / fraction2)
